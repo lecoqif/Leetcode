7d1fa994bad0a723a01f9493542bb81aa50725c6
Abhay Ravindran <abhaydais@gmail.com>
2022-02-22 23:34:54 +0000

    Time: 265 ms (67.36%), Space: 21.1 MB (59.28%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -5,21 +5,18 @@ class Solution:
         
         left = [0 for _ in range(n)]
         
-        right = left[:]
-        
         left[0] = 1
         
         for i in range(1, n):
             left[i] = left[i - 1] * nums[i - 1]
-        
-        right[n - 1] = 1
+
+        right = nums[n - 1]
         
         for i in range(n - 2, -1, -1):
-            right[i] = right[i + 1] * nums[i + 1]
+            left[i] *= right
+            right *= nums[i]
         
-        ret = [left[i] * right[i] for i in range(n)]
-        
-        return ret
+        return left
             
         
         
