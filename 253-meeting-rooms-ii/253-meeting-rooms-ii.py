159cf95cbda2baf98d4363298e3188de61d9cbce
Abhay Ravindran <abhaydais@gmail.com>
2022-03-13 18:25:25 +0000

    Time: 80 ms (89.21%), Space: 17.6 MB (29.64%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -0,0 +1,24 @@
+from heapq import heappush, heappop
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        ret = 1
+        
+        intervals.sort(key=lambda x:x[0])
+        
+        rooms = [(intervals[0][1], intervals[0])]
+        
+        for i in range(1, len(intervals)):
+            end_time, interval = rooms[0]
+            if intervals[i][0] >= end_time:
+                heappop(rooms)
+            
+            heappush(rooms, (intervals[i][1], intervals[i]))
+            ret = max(ret, len(rooms))
+        
+        return ret
+            
+            
+        
+        
+        
