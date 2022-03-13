47e7a78f6d9a8bf3bb8084de5e47e3b82c5f6661
Abhay Ravindran <abhaydais@gmail.com>
2022-03-13 17:32:27 +0000

    Time: 212 ms (55.12%), Space: 15.2 MB (5.80%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,34 @@
+from collections import Counter, defaultdict
+
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        
+        ldict = Counter(s)
+        
+        fdict = defaultdict(list)
+        
+        for l in ldict.keys():
+            fdict[ldict[l]].append(l)
+            
+        max_key = max(fdict.keys())
+        
+        ret = 0
+        
+        while max_key > 0:
+            while len(fdict[max_key]) > 1:
+                curr = fdict[max_key].pop()
+                if max_key > 1:
+                    fdict[max_key - 1].append(curr)
+                ret += 1
+                
+            max_key -= 1
+        
+        return ret
+                
+                       
+                       
+            
+            
+            
+         
+        
