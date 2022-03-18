0c7bf01fe4a2452796e36b0a2b28520b7bcbbc14
Abhay Ravindran <abhaydais@gmail.com>
2022-03-18 14:48:31 +0000

    Time: 76 ms (87.19%), Space: 13.9 MB (74.84%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -5,30 +5,21 @@ class Solution:
         
         s1_char_count = [0 for _ in range(26)]
         
-        n = len(s2)
-        
         s2_char_count = s1_char_count[:]
         
-        for ch in s1:
-            s1_char_count[self.get_ord(ch)] += 1
-        
         for i in range(len(s1)):
+            s1_char_count[self.get_ord(s1[i])] += 1
             s2_char_count[self.get_ord(s2[i])] += 1
         
-        if s1_char_count == s2_char_count:
-                return True
-        
-        while i < n - 1:
-            i += 1
-            
-            s2_char_count[self.get_ord(s2[i])] += 1
-            s2_char_count[self.get_ord(s2[i - len(s1)])] -= 1
+        for i in range(len(s2) - len(s1)):
             
             if s1_char_count == s2_char_count:
-                return True
+                    return True
             
+            s2_char_count[self.get_ord(s2[i + len(s1)])] += 1
+            s2_char_count[self.get_ord(s2[i])] -= 1
             
-        return False
+        return s1_char_count == s2_char_count
             
     def get_ord(self, ch):
         return ord(ch) - ord('a')
