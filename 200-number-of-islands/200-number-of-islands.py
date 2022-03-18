5968c3d73e834c2c4a2b76cc3e741e077da6070e
Abhay Ravindran <abhaydais@gmail.com>
2022-03-18 15:22:09 +0000

    Time: 659 ms (9.08%), Space: 16.2 MB (98.35%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,24 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
-        def dfs(row, col):
-            if not (0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col] == "1"):
-                return
-            
-            grid[row][col] = "0"
-            
-            ranges = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-            
-            for way in ranges:
-                dfs(row + way[0], col + way[1])
-        
-        
         ret = 0
+        
+        deq = deque()
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
                     ret += 1
-                    dfs(i, j)
+                    
+                    deq.append((i, j))
+                    
+                    while len(deq) > 0:
+                        row, col = deq.popleft()
+                        
+                        if not (0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col] == "1"):
+                            continue
+                            
+                        grid[row][col] = "0"
+                        
+                        for way in [[0, 1], [1, 0], [-1, 0], [0, -1]]:
+                            deq.append((row + way[0], col + way[1]))
         
         return ret
+                        
+                    
                 
