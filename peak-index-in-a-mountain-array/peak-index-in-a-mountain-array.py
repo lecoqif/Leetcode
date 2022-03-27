ba64fec8e07d0fda63d49b765d2fbbcbf904b2eb
Abhay Ravindran <abhaydais@gmail.com>
2022-03-27 03:57:18 +0100

    Time: 114 ms, Memory: 15.2 MB - LeetHub

---
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def peakIndexInMountainArray(self, A: List[int]) -> int:
+        
+        def gold1(l, r):
+            return l + int(round((r - l) * 0.382))
+        
+        def gold2(l, r):
+            return l + int(round((r - l) * 0.618))
+        
+        start, end = 0, len(A) - 1
+        
+        x1, x2 = gold1(start, end), gold2(start, end)
+
+        while x1 < x2:
+            if A[x1] < A[x2]:
+                start = x1
+                x1 = x2
+                x2 = gold2(start, end)
+            
+            else:
+                end = x2
+                x2 = x1
+                x1 = gold1(start, end)
+        
+        return A.index(max(A[start:end + 1]))
+           
