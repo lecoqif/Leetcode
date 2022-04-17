42d3752433bfb77ec260eac3f8be9e0561cfe817
Abhay Ravindran <abhaydais@gmail.com>
2022-04-17 23:28:06 +0100

    Time: 722 ms (74.30%), Space: 30.4 MB (34.39%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,29 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        output = []
+        
+        def isPalindrome(word: str) -> bool:
+            if len(word) == 0: return False
+            
+            for i in range(len(word) // 2):
+                if word[i] != word[len(word) - 1 - i]:
+                    return False
+            
+            return True
+        
+        def dfs(word: str, curr: List[str]) -> None:
+            if len(word) == 0:
+                output.append(curr[:])
+                return
+            
+            for i in range(len(word)):
+                currWord = word[0:i + 1]
+                if isPalindrome(currWord):
+                    curr.append(currWord)
+                    dfs(word[i + 1:], curr)
+                    curr.pop()
+        
+        dfs(s, [])
+        return output
+                    
