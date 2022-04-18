e30d68667194e16fae2a981017066a2007eeb891
Abhay Ravindran <abhaydais@gmail.com>
2022-04-18 20:14:07 +0100

    Time: 82 ms (94.13%), Space: 14.2 MB (51.23%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -2,29 +2,29 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         lis = []
         
-        for num in nums:
-            place = self.binSearch(lis, num) 
+        def findInsertPos(arr: List[int], val: int) -> int:
+            start, end = 0, len(arr) - 1
             
-            if place == len(lis):
+            while start <= end:
+                mid = start + (end - start) // 2
+                
+                if arr[mid] >= val:
+                    end = mid - 1
+                
+                else:
+                    start = mid + 1
+            
+            return start
+             
+        for i, num in enumerate(nums):
+            insertPos = findInsertPos(lis, num)
+            if insertPos == len(lis):
                 lis.append(num)
             
             else:
-                lis[place] = num
-            
+                lis[insertPos] = num
+        
         return len(lis)
-    
-    def binSearch(self, nums, val):
-        start, end = 0, len(nums) - 1
-        
-        while start <= end:
-            mid = start + (end - start) // 2
             
-            if nums[mid] < val:
-                start = mid + 1
-                
-            else:
-                end = mid - 1
-        
-        return start
-                
-        
+            
+    
