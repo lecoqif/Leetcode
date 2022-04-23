0027b9f09033795bcbada2bf8d3c5874527681ef
Abhay Ravindran <abhaydais@gmail.com>
2022-04-24 00:40:29 +0100

    Time: 50 ms (40.69%), Space: 13.9 MB (70.21%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.py
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.py
@@ -2,20 +2,21 @@ from typing import Dict
 class Codec:
     
     def __init__(self):
-        self.mapping: Dict[str, str] = {}
+        self.mapping: Dict[int, str] = {}
+        self.counter = 0
 
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        shortUrlSuffix = str(hash(longUrl))
-        self.mapping[shortUrlSuffix] = longUrl
-        return "http://tinyurl.com/" + shortUrlSuffix
+        self.mapping[self.counter] = longUrl
+        self.counter += 1
+        return "http://tinyurl.com/" + str(self.counter - 1)
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
         suffix = shortUrl.replace("http://tinyurl.com/", "")
-        return self.mapping[suffix]
+        return self.mapping[int(suffix)]
         
 
 # Your Codec object will be instantiated and called as such:
