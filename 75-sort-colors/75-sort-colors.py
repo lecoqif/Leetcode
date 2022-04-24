e737c71e6bc63f239ffe51bf6ce3e2bd1bc60b36
Abhay Ravindran <abhaydais@gmail.com>
2022-04-24 20:55:57 +0100

    Time: 30 ms (94.70%), Space: 13.9 MB (65.83%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -4,21 +4,20 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        
-        left, curr, right = 0, 0, n - 1
+        left = curr = 0
+        right = n - 1
         
         while curr <= right:
             if nums[curr] == 2:
                 nums[curr], nums[right] = nums[right], nums[curr]
                 right -= 1
-                
+            
             elif nums[curr] == 0:
                 nums[curr], nums[left] = nums[left], nums[curr]
-                left += 1
                 curr += 1
+                left += 1
             
             else:
                 curr += 1
-            
-                
+        
         
