3fb2375b729788b6c0eb4c3eca6300fb924bb742
Abhay Ravindran <abhaydais@gmail.com>
2022-04-24 20:07:46 +0100

    Time: 132 ms (38.98%), Space: 49.2 MB (5.24%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -9,48 +9,27 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
         
-        slow = fast = head
-        prehead = ListNode(0)
-        prehead.next = head
+        if not head: return
         
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            prehead = prehead.next
+        def reorder(root: ListNode, curr: ListNode) -> ListNode:
+            if not curr: return root
             
-        if fast:
-            slow = slow.next
-            prehead = prehead.next
+            root = reorder(root, curr.next)
+            
+            if not root: return None
+            
+            if root == curr or root.next == curr:
+                curr.next = None
+            
+            else: 
+                root.next, curr.next = curr, root.next
+            
+            return curr.next
+                
         
-        prehead.next = None
+        reorder(head, head.next)
         
-        last = Solution.reverse(slow)
-        
-        curr = head
-        
-        while curr and last:
-            after = curr.next
-            curr.next = last
-            prevLast = last.next
-            last.next = after
-            curr = after
-            last = prevLast
-        
-        return head
     
-    @staticmethod
-    def reverse(head: ListNode) -> ListNode:
-        
-        prev = None
-        curr = head
-        
-        while curr:
-            after = curr.next
-            curr.next = prev
-            prev = curr
-            curr = after
-        
-        return prev
         
         
         
