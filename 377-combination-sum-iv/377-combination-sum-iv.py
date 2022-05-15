1a352ceebcfadb916000dc5c84fb03950761ed3a
Abhay Ravindran <abhaydais@gmail.com>
2022-05-15 23:03:16 +0100

    Time: 49 ms (62.92%), Space: 14.1 MB (29.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -1,18 +1,20 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         
-        @lru_cache(maxsize=None)
-        def backtrack(remain: int) -> None:
-            if remain == 0:
+        @lru_cache(None)
+        def backtrack(curr: int) -> int:
+            
+            if curr == 0:
                 return 1
             
-            result = 0
+            count = 0
             
             for i in nums:
-                if remain - i >= 0:
-                    result += backtrack(remain - i)
-            
-            return result
-            
-            
+                if curr - i >= 0: count += backtrack(curr - i)
+                
+            return count
+        
         return backtrack(target)
+            
+
+                
