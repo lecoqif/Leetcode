c51dad900a0216654d0e95734a858e98f3e429b8
Abhay Ravindran <abhaydais@gmail.com>
2022-05-15 23:14:30 +0100

    Time: 756 ms (36.66%), Space: 14.3 MB (89.07%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.py
@@ -0,0 +1,37 @@
+from collections import Counter, defaultdict
+from heapq import heappush, heappop
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        
+        if n == 0: return len(tasks)
+        
+        counter = Counter(tasks)
+        
+        heap = []
+        
+        for task, count in counter.items():
+            heappush(heap, (-count, task, count))
+        
+        hm = {}
+        
+        ret = 0
+        
+        while hm or heap:
+            ret += 1
+            
+            if heap:
+                _, task, count = heappop(heap)
+                if count > 1: hm[ret + n] = (task, count - 1)
+            
+            if ret in hm:
+                task, count = hm[ret]
+                heappush(heap, (-count, task, count))
+                del hm[ret]
+        
+        return ret
+            
+            
+            
+            
+        
+        
