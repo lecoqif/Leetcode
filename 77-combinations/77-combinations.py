31db2abdbf9152515bb4c9bcae60ba944a7cf145
Abhay Ravindran <abhaydais@gmail.com>
2022-05-15 22:19:16 +0100

    Time: 756 ms (17.33%), Space: 16 MB (49.91%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,4 +1,19 @@
-from itertools import combinations
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return combinations(range(1, n + 1), k)
+        ret = []
+        
+        def backtrack(curr: List[int], start: int, k: int) -> None:
+            
+            if k == 0:
+                ret.append(curr)
+                return
+            
+            for i in range(start, n + 1):
+                curr.append(i)
+                backtrack(curr[:], i + 1, k - 1)
+                curr.pop()
+        
+        backtrack([], 1, k)
+        
+        return ret
