3d18704a6fb8fbb87f23c653c6a40ffeda2f89fe
Abhay Ravindran <abhaydais@gmail.com>
2022-07-28 15:21:25 +0100

    Time: 107 ms (13.95%), Space: 14.5 MB (34.60%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,3 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return ''.join(sorted(s)) == ''.join(sorted(t))
+        
+        if len(s) != len(t): return False
+        
+        charFreq = [0] * 26
+        
+        for i in range(len(s)):
+            charFreq[ord(s[i]) - ord('a')] += 1
+            charFreq[ord(t[i]) - ord('a')] -= 1
+        
+        for i in range(26):
+            if charFreq[i] > 0:
+                return False
+        
+        return True
+        
