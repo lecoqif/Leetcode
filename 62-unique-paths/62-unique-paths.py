e54f498d0de63b8c1858a23e60a1e75db3e15719
Abhay Ravindran <abhaydais@gmail.com>
2022-08-01 22:50:52 +0100

    Time: 54 ms (37.88%), Space: 14 MB (32.95%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -8,19 +8,8 @@ class Solution:
         
         dp = [[1 for _ in range(n)] for i in range(m)]
         
-#         dp[0][0] = 1
-        
-#         def grid_val(row: int, col: int) -> int:
-#             if not (0 <= row < m and 0 <= col < n):
-#                 return 0
-            
-#             return dp[row][col]
-        
         for i in range(1, m):
             for j in range(1, n):
-
-                # if i == 0 and j == 0: 
-                #     continue
                     
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
         
