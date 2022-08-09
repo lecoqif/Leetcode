4113730a6b20b0a9b1eb31e0a10dec9e298ce2eb
Abhay Ravindran <abhaydais@gmail.com>
2022-08-09 19:25:34 +0100

    Time: 131 ms (82.00%), Space: 14.2 MB (83.70%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,30 +1,14 @@
+from bisect import bisect_left
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         lis = []
         
-        def findInsertPos(arr: List[int], val: int) -> int:
-            start, end = 0, len(arr) - 1
-            
-            while start <= end:
-                mid = start + (end - start) // 2
-                
-                if arr[mid] >= val:
-                    end = mid - 1
-                
-                else:
-                    start = mid + 1
-            
-            return start
-             
-        for i, num in enumerate(nums):
-            insertPos = findInsertPos(lis, num)
-            if insertPos == len(lis):
+        for num in nums:
+            val = bisect_left(lis, num)
+            if val == len(lis):
                 lis.append(num)
-            
             else:
-                lis[insertPos] = num
+                lis[val] = num
         
         return len(lis)
             
-            
-    
