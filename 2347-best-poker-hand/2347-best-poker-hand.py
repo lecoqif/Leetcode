bed6904314fff6cec26f7cfe1b1cf4006fc3846a
Abhay Ravindran <abhaydais@gmail.com>
2022-08-23 23:13:29 +0100

    Time: 50 ms (25.49%), Space: 13.9 MB (64.53%) - LeetHub

--- 2347-best-poker-hand/2347-best-poker-hand.py
+++ 2347-best-poker-hand/2347-best-poker-hand.py
@@ -1,18 +1,19 @@
 class Solution:
     def bestHand(self, ranks: List[int], suits: List[str]) -> str:
-        is_flush = all(suits[i] == suits[0] for i in range(5))
+        is_flush = all([suits[i] == suits[0] for i in range(5)])
         
         if is_flush: return "Flush"
         
         counter = Counter(ranks)
         
         for rank, freq in counter.most_common():
-            if freq >= 3:
-                return "Three of a Kind"
             
-            elif freq == 2:
-                return "Pair"
+            if freq >= 3: return "Three of a Kind"
+            
+            if freq == 2: return "Pair"
         
         return "High Card"
-        
+                
+                        
+                        
         
