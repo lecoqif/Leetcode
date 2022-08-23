6575cd9e31494fa83d0b9e93b07c82590f091a03
Abhay Ravindran <abhaydais@gmail.com>
2022-08-23 22:42:37 +0100

    Time: 91 ms (39.59%), Space: 14 MB (64.01%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -1,6 +1,6 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        true_start, false_start, true_count, false_count = True, False, 0, 0
+        true_start, true_count = True, 0
         
         for i, ch in enumerate(s):
             val = int(ch)
@@ -8,10 +8,7 @@ class Solution:
             if true_start != val:
                 true_count += 1
             
-            if false_start != val:
-                false_count += 1
-            
             true_start = not true_start
-            false_start = not false_start
+
         
-        return min(true_count, false_count)
+        return min(true_count, len(s) - true_count)
