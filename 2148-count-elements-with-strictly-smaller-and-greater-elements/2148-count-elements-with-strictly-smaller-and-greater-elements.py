cbd52693df80db13de737bcb3a00a4c000c2d833
Abhay Ravindran <abhaydais@gmail.com>
2022-08-25 22:35:18 +0100

    Time: 63 ms (56.10%), Space: 14 MB (19.07%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
@@ -1,12 +1,13 @@
 class Solution:
     def countElements(self, nums: List[int]) -> int:
         
-        maxNum, minNum = max(nums), min(nums)
-
+        nums.sort()
+        
+        minNum, maxNum = nums[0], nums[-1]
         ret = 0
         
-        for num in nums:
-            if num < maxNum and num > minNum:
+        for num in nums[1:-1]:
+            if num > minNum and num < maxNum:
                 ret += 1
         
         return ret
