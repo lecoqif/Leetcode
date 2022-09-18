59a60177dc42a4e5aa6ef0fbdea2869d834c7e4b
Abhay Ravindran <abhaydais@gmail.com>
2022-09-18 22:02:06 +0100

    Time: 3665 ms (40.94%), Space: 53.9 MB (53.90%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
@@ -8,15 +8,15 @@ class Solution:
         
         ret = 1
         
-        heapq.heappush(heap, (intervals[0][1], intervals[0]))
+        heapq.heappush(heap, intervals[0][1])
         
         for interval in intervals[1:]:
-            top_end, top_interval = heap[0]
+            top_end = heap[0]
             
             if interval[0] > top_end:
                 heapq.heappop(heap)
             
-            heapq.heappush(heap, (interval[1], interval))
+            heapq.heappush(heap, interval[1])
             
             ret = max(ret, len(heap))
         
